92cd9015cd55cf5173cf8128a07033635006b7d5
Victor Gabriel <victorgabryel@hotmail.com>
2016-01-08 23:24:26 -0200

    Realiza inscrição

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -4,5 +4,5 @@ from django import forms
 class SubscriptionForm(forms.Form):
     name = forms.CharField(label='Nome')
     cpf = forms.CharField(label='CPF')
-    email = forms.EmailField(label='Email')
+    email = forms.EmailField(label='email')
     phone = forms.CharField(label='Telefone')
